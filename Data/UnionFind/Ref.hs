0fe2193c5c7de607992837eafa3458953e432f06
kputnam <235798+kputnam@users.noreply.github.com>
2014-08-06 14:03:44 -0500

    Implement 'union' and 'connected'

--- Data/UnionFind/Ref.hs
+++ Data/UnionFind/Ref.hs
@@ -1,5 +1,6 @@
 module Data.UnionFind.Ref
-  ( singleton
+  ( Class ( label )
+  , singleton
   , union
   , find
   , connected
@@ -14,8 +15,7 @@ data Class s a =
   Class
   { label  :: a
   , parent :: STRef s (Maybe (Class s a))
-  , size   :: STRef s Int
-  }
+  , size   :: STRef s Int }
 
 -- Elements in the graph are uniquely identified by their `parent` field, which
 -- is a reference to another Class. We're not comparing what is being referred
@@ -28,23 +28,42 @@ data Class s a =
 instance Eq (Class s a) where
   a == b = parent a == parent b
 
--- Non-optional reference to a Class (dereferencing it never results in Nothing)
-type Ref s a
-  = STRef s (Class s a)
-
 singleton :: a -> ST s (Class s a)
 singleton a = Class a <$> newSTRef Nothing <*> newSTRef 1
 
 -- | Returns a reference to the representative element of @k@'s equivalence class.
---   The given reference may be updated (to flatten indirect references). Runs in
---   O(1) amortized time.
-find :: Ref s a -> ST s (Ref s a)
+--   The given class's `parent` reference may be updated (to reduce indirection).
+--   Runs in O(1) amortized time.
+find :: Class s a -> ST s (Class s a)
 find k = undefined
 
 -- | Join the equivalence classes of @j@ and @k@, and return a reference to the
---   element representing the class containing @j@ and @k@. Runs in O(1) amortized time.
-union :: Class s a -> Class s a -> ST s (Ref s a)
-union j k = undefined
+--   element representing the class containing @j@ and @k@. Runs in O(1) amortized
+--   time.
+union :: Class s a -> Class s a -> ST s (Class s a)
+union j k
+  | j == k    = return j
+  | otherwise = do
+      rootJ <- find j
+      rootK <- find k
+
+      -- Check if these are disjoint classes
+      if rootJ == rootK
+      then
+        sizeJ <- readSTRef (size rootJ)
+        sizeK <- readSTRef (size rootK)
+
+        -- Update the smaller class to point to the larger
+        case sizeJ `compare` sizeJ of
+          LT -> link rootJ rootK (sizeJ + sizeK)
+          _  -> link rootK rootJ (sizeJ + sizeK)
+      else return rootJ
+  where
+    link :: Class s a -> Class s a -> Int -> ST s (Class s a)
+    link small large n = do
+      writeSTRef (parent small) (Just large)
+      writeSTRef (size large) n
+      return large
 
 -- | @True@ if @j@ and @k@ belong to the same equivalence class. Runs
 --   in O(1) amortized time.
@@ -52,18 +71,15 @@ connected :: Class s a -> Class s a -> ST s Bool
 connected j k
   | j == k    = return True -- trivial case
   | otherwise = do
-      -- Maybe (Ref s a)
-      parentJ <- deref (parent j)
-      parentK <- deref (parent k)
+      -- Maybe (Class s a)
+      parentJ <- readSTRef (parent j)
+      parentK <- readSTRef (parent k)
 
       -- If either parent is Nothing, then j or k were unequal representative elements
       -- (remember, we know j /= k from earlier). Otherwise, see if they refer to the
       -- same representative element.
       fromMaybe (return False) (compare <$> parentJ <*> parentK)
   where
-    deref :: STRef s (Maybe (Class s a)) -> ST s (Maybe (Ref s a))
-    deref = undefined
-
-    compare :: Ref s a -> Ref s a -> ST s Bool
+    compare :: Class s a -> Class s a -> ST s Bool
     compare j k = (==) <$> find j <*> find k
 
